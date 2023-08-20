e99527e9dc55cf49e66730b43a71998c42d1d0ad
DzungDang <ddbdzung.wa@gmail.com>
2023-08-21 01:48:44 +0700

    :hammer: Fix TODO - refs #8

--- src/modules/users/validations/get-user.validation.ts
+++ src/modules/users/validations/get-user.validation.ts
@@ -2,7 +2,6 @@ import * as Joi from 'joi';
 
 import { objectId } from './custom-user.validation';
 
-// TODO: Rename to getUserParams
-export const GetUserSChema = Joi.object({
+export const getUserParams = Joi.object({
   id: Joi.string().required().custom(objectId),
 });
